fa62f9763508521e03da881ce2ab869882e43fd4
Tatsunori Uchino <tats.u@live.jp>
2018-12-06 17:04:23 +0900

    Add self-build script for Windows EXE

---
+++ scripts/build_windows_exe.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop"
+pyinstaller easy_install.py --onefile
+if(Test-Path .\easy_install.exe) {
+    Remove-Item .\easy_install.exe
+}
+Move-Item dist\easy_install.exe .
+Remove-Item -Recurse build
+Remove-Item -Recurse dist
+remove-item easy_install.spec
